c44ec77b629364793d5fbded6da10539e5991490
RublevD <danil.r@devhouse.pro>
2020-03-21 01:03:49 +0300

    Very very simple nav and screens

--- src/components/screens/Screen.tsx
+++ src/components/screens/Screen.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { View } from 'react-native';
 
-const Screen = () => {
-  return <View>Heeey</View>;
+const Screen: React.FC<{ children: any }> = ({ children }) => {
+  return <View>{children}</View>;
 };
 
 export default Screen;
